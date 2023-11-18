f3ee220e189132649c1ed21ac74909d86f157d20
Filipe Neves <filipe.nevser@gmail.com>
2023-11-18 20:12:25 -0300

    feat: change config to deploy in gh pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://filipenevs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/gincumentation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'filipenevs', // Usually your GitHub org/user name.
+  projectName: 'gincumentation', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -52,7 +52,7 @@ const config: Config = {
         docs: {
           sidebarPath: './sidebars.ts',
           editUrl:
-            'https://github.com/filipenevs/gincumentation/',
+            'https://github.com/filipenevs/gincumentation/tree/main',
         },
         theme: {
           customCss: './src/css/custom.css',
